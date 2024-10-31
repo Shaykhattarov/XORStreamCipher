435df9cfb4d6c2eca1eb53d77200da976af848ad
Ildan <ildan201@yandex.ru>
2024-10-31 14:08:33 +0400

    fix bug with cyryllic encoding and maximize size of text and password

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from utils import StreamCipher
 class App:
 
     root: Tk
-    data: str|bytes = ''
+    data: str|bytes
     password: str = ''
     cipher: str = ''
     datatype: str = 'text'
@@ -25,7 +25,7 @@ class App:
         self.root.geometry("500x600")
 
     def __create_screen_elements(self):
-        common_label: tk.Label = tk.Label(self.root, text="Поточное шифрование\n(кириллица не поддерживается)")
+        common_label: tk.Label = tk.Label(self.root, text="Поточное шифрование")
         common_label.pack(pady=[10, 25])
 
         ##
@@ -39,6 +39,7 @@ class App:
         open_textfile: ttk.Button = ttk.Button(text="Выбрать файл", command=self.__openfile)
         open_textfile.pack(pady=5)
 
+        # print(len(self.data))
         label_password: tk.Label = tk.Label(self.root, text="Пароль для шифрования:")
         label_password.pack(pady=5)
 
@@ -48,6 +49,7 @@ class App:
         self.label_error: tk.Label = tk.Label(self.root, text='')
         self.label_error.pack()
 
+        # print(len(self.data))
         encrypt_text: ttk.Button = ttk.Button(text="Зашифровать данные", width=25, command=self.__encrypt)
         encrypt_text.pack(pady=[5, 30])
 
@@ -70,9 +72,7 @@ class App:
 
         decrypt_text: ttk.Button = ttk.Button(text="Дешифровать данные", width=25, command=self.__decrypt)
         decrypt_text.pack(pady=5)
-        return
-
-
+        
     def __openfile(self):
         """ Функция чтения файла с шифруемым текстом """
         filepath = filedialog.askopenfilename(title="Выбор файла", initialdir="./data/", defaultextension='txt', initialfile="")
@@ -87,22 +87,25 @@ class App:
                 self.datatype = "bytes"
                 with open(filepath, 'rb') as file:
                     self.data: bytes = file.read()
+                    print(len(self.data))
                     self.scrolled_text.delete("1.0", tk.END)
                     self.scrolled_text.insert("1.0", "Бинарные данные из файла считаны!")
-
+             
     def __encrypt(self) -> None:
         """ Функция шифрования текста """
-        self.data: str = self.scrolled_text.get("1.0", tk.END)
+        if self.data is None:
+            self.data: str = self.scrolled_text.get("1.0", tk.END)
+            self.data = self.data[:len(self.data) - 1]
         self.password: str = self.entry_password.get()
 
-        if len(self.data) <= 4 or len(self.password) < 2:
+        if len(self.data) < 2 or len(self.password) < 2:
             self.label_error['text'] = "Пустые данные или пароль"
             return
 
         cryptographer: StreamCipher = StreamCipher()
         encrypted_data: str = cryptographer.encrypt(self.data, self.password)
-
-        with open('./encrypted.txt', 'w+', encoding='utf-8') as file:
+      
+        with open('./data/encrypted.txt', 'w+', encoding='utf-8') as file:
             file.write(encrypted_data)
 
         self.label_error['text'] = "Данные зашифрованы"
@@ -111,8 +114,8 @@ class App:
     def __decrypt(self) -> None:
         self.cipher: str = self.scrolled_cipher.get("1.0", tk.END)
         self.password: str = self.entry_decrypt_password.get()
-
-        if len(self.cipher) <= 8 or len(self.password) < 2:
+        self.cipher = self.cipher[:len(self.cipher) - 1]
+        if len(self.cipher) < 2 or len(self.password) < 2:
             self.label_decrypt_error['text'] = "Пустые данные или пароль"
             return
 
@@ -120,25 +123,33 @@ class App:
         try:
             decrypted_data: str|bytes = cryptographer.decrypt(self.cipher, self.password, self.datatype)
         except Exception as err:
-            self.label_decrypt_error['text'] = "Не удалось дешифровать (не верный пароль или было зашифровано сообщение на кириллице)"
+            self.label_decrypt_error['text'] = "Не удалось дешифровать! \n(не верный пароль)"
             print(err)
             return
 
         if self.datatype == 'text':
-            with open('./decrypted.txt', 'w', encoding='utf-8') as file:
+            with open('./data/decrypted.txt', 'w+', encoding='utf-8') as file:
                 file.write(decrypted_data)
         else:
-            with open('./decrypted.png', 'wb') as file:
+            with open('./data/decrypted.png', 'wb+') as file:
                 file.write(decrypted_data)
-
+        self.data = None
         self.label_decrypt_error['text'] = "Данные дешифрованы"
         return 
 
     def run(self) -> None:
         self.root.mainloop()
 
-
+    def test(self):
+        cryptographer = StreamCipher()
+        data = "Привет мир!"
+        password = 'GXmx1is7yNz53D8oe1uiF~OCar8K1OoP}ZL01HgR2p$UbJiVEV*ik%bCyJ5%?*R0'
+        ciph = cryptographer.encrypt(data, password)
+        res = cryptographer.decrypt(ciph, password)
+        print(ciph, ' -> ', res, ' - ', password)
 
 if __name__ == "__main__":
     app = App()
+    app.test()
     app.run()
+    
--- utils/cipher.py
+++ utils/cipher.py
@@ -3,6 +3,8 @@ from utils import Hash
 
 # Пароль -> Hash -> Затравка -> Генератор ГПСЧ -> Гамма -> Шифр <- Входной текст
 
+ENCODING = "UTF-8"
+
 class StreamCipher:
 
     seed: str = 'secret'
@@ -12,51 +14,52 @@ class StreamCipher:
         self.prng = GeneratorBBS()
 
     def encrypt(self, text: str|bytes, password: str) -> str:
-        if len(password) > 64 or len(password) <= 2:
-            raise Exception("Длина текста должна быть больше 2 байт и меньше 64 байт")
-        if len(text) > 6144 or len(text) < 4 :
-           raise Exception("Длина текста должна быть больше 4 байт и меньше 6144 байт")
-        if len(text) < len(password):
-            raise Exception("Размер пароля должна быть меньше размера данных")
-
-        pswhash: int = self.__hashpassword(self.seed, password) # генерируем хеш из пароля и затравки
-        textlength: int = len(text) # размер файла в кол-ве байтов
-
-        rndsequence: list[int] = self.prng.generate(pswhash, (textlength * 8))
-        rndsequence: int = self.__digestrandomsequence(rndsequence) # представляем ее числом
-        text: int = self.__digesttext(text)
-        gamma: int = self.__gamming(pswhash, rndsequence) # гаммируем хеш-пароль и ПСЧ (0100101001...)
-
-        return format(text ^ gamma, 'X')
+        if len(password) < 2 or len(text) < 2:
+            raise Exception("Длина текста и пароля должна быть больше 2 байт")
+        textbytes: bytearray = self.__converttexttobytearray(text)
+        gamma: bytearray = self.__generate_gamma(password, len(textbytes))
+        cipher: bytearray = self.__xor(textbytes, gamma)
+        return cipher.hex() # преобразуем к виду в 16-ричном формате
     
-    def decrypt(self, cipher: str, password: str, datatype: str='text'):
-        cipher: int = int(cipher, 16)
-        datalength: int = len(bin(cipher)[1:]) // 8
-        pswhash: int = self.__hashpassword(self.seed, password) # генерируем хеш из пароля и затравки
-        rndsequence: list[int] = self.prng.generate(pswhash, (datalength * 8))
-        rndsequence: int = self.__digestrandomsequence(rndsequence)
-        gamma: int = self.__gamming(pswhash, rndsequence)
-        intdata: int = cipher ^ gamma
-        bytesdata: bytes = int.to_bytes(intdata, length=datalength, byteorder='big', signed=False)
+    def decrypt(self, cipher: str, password: str, datatype: str='text') -> str|bytearray:
+        cipher: bytes = bytes.fromhex(cipher)
+        gamma: bytearray = self.__generate_gamma(password, len(cipher))
+        decrypted: bytearray = self.__xor(cipher, gamma)
+        
         if datatype.lower() == 'text':
-            return bytesdata.decode('UTF-8')
-        else:
-            return bytesdata
+            return decrypted.decode('UTF-8')
+        return decrypted
+        
+    def __generate_gamma(self, password: str, datalength: int) -> bytearray:
+        pswhash: int = self.__hashpassword(self.seed, password) # генерируем хеш из пароля и затравки
+        rndsequence: bytearray = self.__convertsequencetobytearray(self.prng.generate(pswhash, (datalength * 8))) # генерируем последовательность с помощью хеша пароля и преобразуем ее в bytearray длиной datalength
+        pswhash: bytes = int.to_bytes(pswhash, length=len(rndsequence), byteorder='big', signed=False) # преобразование хеша пароля в bytes длиной datalength байт
+        return [(pswhash[ind] | rndsequence[ind]) for ind in range(datalength)] # производим побитовое сложение байтов и возвращаем результат 
+
+    def __converttexttobytearray(self, data: str|bytes) -> bytearray:
+        if isinstance(data, str): return bytearray(data.encode(ENCODING))
+        if isinstance(data, bytes): return bytearray(data)
+    
+    def __convertsequencetobytearray(self, sequence: list[int]) -> bytearray:
+        buffer = ""
+        result = bytearray()
+        for i in range(len(sequence)):
+            buffer += str(sequence[i])
+            if len(buffer) % 8 == 0:
+                result.append(int(buffer, 2))
+                buffer = ""
+        return result
 
     def __hashpassword(self, seed: str, password: str) -> str:
         return self.hasher.mahash5(password + seed)
         
-    def __digesttext(self, text: str|bytes) -> int:
-        if isinstance(text, str):
-            text: bytes = text.encode('UTF-8')
-        digesttext: int = int.from_bytes(text, byteorder='big', signed=False)
-        return digesttext
-    
-    def __digestrandomsequence(self, sequence: list[int]) -> str:
-        return int("".join([str(el) for el in sequence]), 2)
-    
-    def __gamming(self, pswhash: int, rndsequence: int) -> int:
-        return pswhash | rndsequence 
+    def __xor(self, text: bytearray, gamma: bytearray) -> bytearray:
+        if len(text) != len(gamma):
+            raise Exception("Длина ПСЧ и длина текста должны быть одинаковыми")
+        result = bytearray()
+        for i in range(len(text)):
+            result.append(text[i] ^ gamma[i])
+        return result
         
 
 # scipher = StreamCipher()
